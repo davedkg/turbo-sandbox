747b2bd99754b522faa38ca30c81b2033293dbed
David Guilfoyle <davedkg@minepi.com>
2024-01-18 01:40:22 -0500

    a forecast is cached for 30 minutes

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -3,6 +3,8 @@
 class Forecast
   include ActiveModel::Model
 
+  CACHE_EXPIRES_IN = 30.minutes
+
   attr_accessor :address, :zip_code, :country # input attributes
   attr_reader :temperature_current, :temperature_max, :temperature_min # output attributes
 
@@ -11,8 +13,16 @@ class Forecast
   validates :zip_code, :country, presence: true, if: -> { errors.empty? }
   validate :populate_current_forecast, if: -> { errors.empty? }
 
+  def cached?
+    @cached
+  end
+
   def populate_current_forecast
-    current_forecast = service.forecast_by_zipcode_and_country(zip_code, country)
+    current_forecast = fetch_cached_forecast
+
+    @cached = !current_forecast.nil? # see cached?
+
+    current_forecast ||= fetch_new_forecast
 
     errors.add(:address, 'has no forecast') unless current_forecast
 
@@ -24,6 +34,24 @@ class Forecast
   end
 
   private
+
+  def fetch_cached_forecast
+    response = Rails.cache.read(cache_key)
+
+    response&.symbolize_keys
+  end
+
+  def fetch_new_forecast
+    response = service.forecast_by_zipcode_and_country(zip_code, country)
+
+    Rails.cache.write(cache_key, response, expires_in: CACHE_EXPIRES_IN)
+
+    response
+  end
+
+  def cache_key
+    @cache_key ||= "forecasts-#{country.downcase}-#{zip_code}"
+  end
 
   def service
     @service ||= ForecastService.new
